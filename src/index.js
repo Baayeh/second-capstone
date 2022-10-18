71b85dabbe821c33583fe794b596ea94bb31e47d
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-10-18 15:00:39 +0000

    Add styling to card

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import countryList from './modules/DOMElements.js';
 // Display the list of countries
 const displayCountries = async () => {
   const countries = await API.getCountries();
-  const sixCountries = countries.slice(0, 9);
+  const sixCountries = countries.slice(0, 6);
   countryList.innerHTML = sixCountries
     .map(
       (country) => `<li class="country-item">
@@ -13,7 +13,7 @@ const displayCountries = async () => {
         <img src="${country.flags.svg}" alt="Ugandan Flag">
       </div>
       <div class="card-body">
-        <h3 class="country-name">${country.name.common}</h3>
+        <p class="country-name mb-0">${country.name.common}</p>
         <div class="likes">
           <a href="" type="button">
             <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
