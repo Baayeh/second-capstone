aa708442a5754d7d795a2568b50a44ad209eb514
Daphineatim <hopedaphine19@gmail.com>
2022-10-21 20:48:36 +0300

    Update comments.js

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -11,7 +11,7 @@ const addComment = async (data) => {
   const response = await fetch(`${BASE_URL}apps/IgY08bvfNLdtRYcSagqI/comments`, {
     method: 'POST',
     headers: { 'Content-type': 'application/json' },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   });
   return response.text();
 };
